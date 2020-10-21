81079043ece1f5a32c2d5fa5eb4dfb0235fad4b0
NahuelBiladoniga <nahuelbiladoniga@gmail.com>
2020-10-20 22:49:05 -0300

    Initial Commit

---
+++ DataAccess/UnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using Domain;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Reflection.Metadata;
+
+namespace DataAccess
+{
+    public class UnitOfWork
+    {
+        public IEnumerable<Client> Clients { get; }
+        public IEnumerable<Comment> Comments { get; }
+        public IEnumerable<Photo> Photos { get; }
+    }
+}
---
+++ TCPComm/Protocol/ProtocolConstants.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TCPComm.Protocol
+{
+    class ProtocolConstants
+    {
+    }
+}
