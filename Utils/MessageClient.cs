c30d30908630ae307150e7ba51dcec12169c9873
NahuelBiladoniga <nahuelbiladoniga@gmail.com>
2020-12-03 02:37:52 -0300

    Client

---
+++ Utils/MessageClient.cs
@@ -0,0 +1,32 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utils
+{
+    public static class MessageClient
+    {
+        public static Task<bool> SendMessage(IModel channel, string message)
+        {
+            bool returnVal;
+            try
+            {
+                var body = Encoding.UTF8.GetBytes(message);
+                channel.BasicPublish(exchange: "",
+                    routingKey: "log_queue",
+                    basicProperties: null,
+                    body: body);
+                returnVal = true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                returnVal = false;
+            }
+
+            return Task.FromResult(returnVal);
+        }
+    }
+}
